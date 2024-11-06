aa20779428c565903758356c9d2cec7c6df0ac2f
Tejasrai9 <162309793+Tejasrai9@users.noreply.github.com>
2024-11-06 16:15:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const connectDB = require('./config/database'); // MongoDB connection
 const offerRoutes = require('./routes/offer');   // Offer letter routes
 
@@ -11,6 +12,7 @@ connectDB();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors()); // Enable CORS if needed
 
 // Serve static files from the 'public' folder
 app.use(express.static('public'));
